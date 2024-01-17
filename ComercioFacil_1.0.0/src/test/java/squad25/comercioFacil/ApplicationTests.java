784108976d99e750cd080f8f8d82a42b35ba47bc
Pedro Goya <pedrogoya2005@outlook.com>
2024-01-16 21:48:18 -0300

    UpdateFrontEntV1

---
+++ ComercioFacil_1.0.0/src/test/java/squad25/comercioFacil/ApplicationTests.java
@@ -0,0 +1,13 @@
+package squad25.comercioFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
